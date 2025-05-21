1ff5a83a529085de158c01c5c281ff7aba9ef96a
henryx76 <henryhesdras349@gmail.com>
2025-05-21 19:14:28 +0000

    subindo aula

---
+++ introduction-js/010-aula.js
@@ -0,0 +1,14 @@
+let password = "tomorrow";
+let userGuess = "";
+
+
+let factorial = 1;
+let number = 5;
+let original = number;
+
+do {
+    factorial = factorial * number;
+    number--
+} while (number > 0);
+
+console.log(original + " factorial is " + factorial);
